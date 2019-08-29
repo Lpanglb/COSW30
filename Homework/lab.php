f43f5f81b31e80820cd4540583d5942a7009c0c6
EC2 Default User <ec2-user@ip-172-31-80-55.ec2.internal>
2019-08-29 02:59:07 +0000

    Create something something something

---
+++ Homework/lab.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html><title>My first PHP Page</title>
+<body>
+    <h1>
+        Welcome to my page
+    </h1>
+        <p>jkdhfjks dhaflhfuiglja hljkdhsadfhajkld hfaljkdsh flkjsad hlgkhdsalkghsaioughwhg laksjhgsakl hgkjlash gkahglepoiqglaks gja ghjkah glshgsh gjklsdahfkljsadh flksa hfkljahsd flkjahsf sldkjf las hflsafj  fsajf laskh flkjsda hfljk sfjk</p>
+    <?php
+        echo 'This is my PHP beautiful paragrah';
+    ?>
+</body>
+</html>
---
+++ Homework/lbc.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head><title>Test Page</title></head>
+    <body>
+        <h1>Hello Class</h1>
+        <p>This a paragrah</p>
+        
+    </body>
+    
+</html>
