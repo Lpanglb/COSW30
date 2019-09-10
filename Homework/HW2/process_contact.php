0d84e1c3eb12b272d65209f571c32958c74b278e
Lpanglb <54652484+Lpanglb@users.noreply.github.com>
2019-09-09 17:26:50 -0700

    Add files via upload

---
+++ Homework/HW2/process_contact.php
@@ -0,0 +1,77 @@
+<?php
+$checkyes = $_POST['checkyes'];
+$constribution = $_POST['constribution'];
+$gender = $_POST['gender'];
+
+$firstname=$_POST['first_name'];
+$lastname=$_POST['last_name'];
+$yes = $_POST['checkboxyes'];
+$comments = $_POST['comments'];
+
+
+//$football = $POST['football'];
+//$skiing = $POST['skiing'];
+//$Swimming = $POST['swimmimg'];
+//$computergame = $POST['computergame'];
+//$gardening = $POST['gardening'];
+//$hiking = $POST['hiking'];
+
+//Condition for Comments
+
+
+
+ if(isset($yes)){
+   
+     
+     echo "<p> Thank you for supporting us </p>";
+     echo "<p> Your First Name  is $firstname </p>";
+     echo "<p> Your Last Name is $lastname </p>";
+     echo "<p> You are $gender </p>";
+    
+   //Selection of interests
+   
+    if(!empty($_POST["interests"])){
+      echo "<p> You have select following interests:</p>";
+    foreach($_POST["interests"] as $interests)
+    echo '<p>'.$interests. '</p>';
+    }
+     else echo "<p> You didn't select Any Interest '</p>";
+  //
+    echo "<p> Your Would like to Constribute for $constribution </p>";
+  //Checking for comments
+     if(!empty($comments)){
+            echo "<p>Thank you for you Comments:</p>";
+      echo "<p>$comments</p>";
+      
+     }
+     else echo "<p> You did not leave any comments</p>";
+     
+    }
+ else{ 
+       echo "<p> You did not want to support us</p>";
+       echo "<p> Your First Name  is $firstname </p>";
+       echo "<p> Your Last Name is $lastname </p>";
+       echo "<p> You are $gender </p>";
+     
+        //Selection of interests
+   
+    if(!empty($_POST["interests"])){
+      echo "<p> You have select following interests:</p>";
+    foreach($_POST["interests"] as $interests)
+    echo '<p>'.$interests. '</p>';
+    }
+     else echo "<p> You didn't select Any Interest </p>";
+  //
+       echo "<p> Your Would like to Constribute for $constribution </p>";
+        //Checking for comments
+     if(!empty($comments)){
+      echo "<p>Thank you for you Comments:</p>";
+      echo "<p>$comments</p>";}
+     else echo "<p> You did not leave any comments</p>";
+     
+   
+ }
+
+
+
+?>
